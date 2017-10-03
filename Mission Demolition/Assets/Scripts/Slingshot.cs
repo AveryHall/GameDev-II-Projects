c7ed4831c6a69182ce17bb367129ee9a000dc76b
Avery Hall <avery7777795@yahoo.com>
2017-10-03 10:46:40 -0500

    Mission Demolition GameLab Update I

---
+++ Mission Demolition/Assets/Scripts/Slingshot.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour {
+    // fields set in the Unity Inspector pane
+    public GameObject prefabProjectile;
+    public bool _____________;  // field sets dynamically
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+
+    private void Awake() {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+
+    }
+
+    void OnMouseEnter() {
+        //print("SlingShot: OnMouseEnter()");
+        launchPoint.SetActive(true);
+    }
+
+    private void OnMouseExit() {
+        //print("Slingshot: OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+
+    private void OnMouseDown() {
+        // The player has pressed the mouse button while over Slingshot
+        aimingMode = true;
+        // Instantiate a Projectile
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        // Start it at launchPoint
+        projectile.transform.position = launchPos;
+        // Set it to is kinematic for now
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
