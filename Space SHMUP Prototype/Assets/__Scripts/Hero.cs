e5a346b242484651ae0d57f00b974185bf54d1cb
Avery Hall <aphall@mail.lipscomb.edu>
2017-10-12 01:17:40 -0500

    Space SHMUP Prototype Release 0.1.1
    Started the book walkthrough

---
+++ Space SHMUP Prototype/Assets/__Scripts/Hero.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour {
+	static public Hero S;
+
+	// These fields control the movement of the ship
+	public float speed = 30;
+	public float rollMult = -45;
+	public float pitchMult = 30;
+
+	// Ship status information
+	public float shieldLevel = 1;
+
+	public bool _________________;
+
+	public Bounds bounds;
+
+	void Awake() {
+		S = this;	// Sets the Singleton
+		bounds = Utils.CombineBoundsOfChildren(this.gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Pull in information from the Input class
+		float xAxis = Input.GetAxis("Horizontal");
+		float yAxis = Input.GetAxis("Vertical");
+
+		// Change the transform.position based on the axes
+		Vector3 pos = transform.position;
+		pos.x += xAxis * speed * Time.deltaTime;
+		pos.y += yAxis * speed * Time.deltaTime;
+		transform.position = pos;
+
+		bounds.center = transform.position;
+
+		// Keep the ship constrained to the screen bounds
+		Vector3 off = Utils.ScreenBoundsCheck(bounds, BoundsTest.onScreen);
+		if (off != Vector3.zero) {
+			pos -= off;
+			transform.position = pos;
+		}
+
+		// Rotate the ship to make it feel more dynamic
+		transform.rotation = Quaternion.Euler(yAxis*pitchMult, xAxis*rollMult, 0);
+
+	}
+}
---
+++ Space SHMUP Prototype/Assets/__Scripts/Utils.cs
@@ -0,0 +1,241 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This is actually
+public enum BoundsTest {
+
+	center,    // Is the center of the GameObject on screen?
+	onScreen,  // Are the bounds entirely on screen?
+	offScreen, // Are the bounds entirely off screen?
+
+}
+
+public class Utils : MonoBehaviour {
+
+	//====================== Bounds Functions ===============================\\
+
+	// Creates bounds that encapsulate the two bounds passed in.
+	public static Bounds BoundsUnion(Bounds b0, Bounds b1) {
+
+		//If the size of one of the bounds is Vector3.zero, ignore that one
+		if (b0.size == Vector3.zero && b1.size != Vector3.zero) {
+			return (b1);
+		} else if (b0.size != Vector3.zero && b1.size == Vector3.zero) {
+			return (b0);
+		} else if (b0.size == Vector3.zero && b1.size == Vector3.zero) {
+			return (b0);
+		}
+
+		//Strech b0 to include the b1.min and b1.max
+		b0.Encapsulate(b1.min);
+		b0.Encapsulate(b1.max);
+		return (b0);
+
+	}
+
+	public static Bounds CombineBoundsOfChildren(GameObject go) {
+
+		//Creates an empty Bounds b
+		Bounds b = new Bounds(Vector3.zero, Vector3.zero);
+
+		//If this GameObject has a Renderer Component...
+		if (go.GetComponent<Renderer> () != null) {
+			//Expand b to contain the Renderer's Bounds
+			b = BoundsUnion (b, go.GetComponent<Renderer> ().bounds);
+		}
+
+		//If this GameObject has a Collider Component...
+		if (go.GetComponent<Collider> () != null) {
+			//Expand b to contain the Collider's Bounds
+			b = BoundsUnion (b, go.GetComponent<Collider> ().bounds);
+		}
+
+		//Recursively iterate through each child of this GameObject.transform
+		foreach (Transform t in go.transform) {
+			//Expand b to contain their Bounds as well
+			b = BoundsUnion(b, CombineBoundsOfChildren(t.gameObject));
+		}
+
+		return (b);
+	}
+
+	// Make a static read-only public property camBounds
+	static public Bounds camBounds {
+		get {
+			// if _camBounds hasn't been set yet
+			if (_camBounds.size == Vector3.zero) {
+				// SetCameraBounds using the default Camera
+				SetCameraBounds ();
+			}
+			return (_camBounds);
+		}
+	}
+
+	// This is a private static field that camBounds uses
+	static private Bounds _camBounds;
+
+	// This function is used by camBounds to set _camBounds and can also be called directly
+	public static void SetCameraBounds (Camera cam=null) {
+		
+		//If no Camera was passed in, use the main Camera
+		if(cam == null) cam = Camera.main; 
+
+		//Note: This makes a few important assumptions about the camera!
+		// 1. The camera is Orthographic
+		// 2. The camera is at a rotation of R: [0, 0, 0]
+
+		//make Vector3s at the topLeft and bottomRight of the Screen coords
+		Vector3 topLeft = new Vector3(0, 0, 0);
+		Vector3 bottomRight = new Vector3(Screen.width, Screen.height, 0);
+
+		//Convert these to world coordinates
+		Vector3 boundTLN = cam.ScreenToWorldPoint(topLeft);
+		Vector3 boundBRF = cam.ScreenToWorldPoint(bottomRight);
+
+		// Adjust their zs to be at the near and far Camera clipping planes
+		boundTLN.z += cam.nearClipPlane;
+		boundBRF.z += cam.farClipPlane;
+
+		// Find the center of the Bounds
+		Vector3 center = (boundTLN + boundBRF) / 2f;
+		_camBounds = new Bounds (center, Vector2.zero);
+
+		// Expand _camBounds to encapsulate the extents
+		_camBounds.Encapsulate(boundTLN);
+		_camBounds.Encapsulate(boundBRF);
+
+	}
+
+	// Check to see whether the Bounds bnd are within the camBounds
+	public static Vector3 ScreenBoundsCheck(Bounds bnd, BoundsTest test = BoundsTest.center) {
+		return(BoundsInBoundsCheck (camBounds, bnd, test));
+	}
+
+	// Checks to see whether Bounds lilB are within Bounds bigB
+	public static Vector3 BoundsInBoundsCheck ( Bounds bigB, Bounds lilB, BoundsTest test = BoundsTest.onScreen) {
+
+		// The behavior of this function is different based on the BoundsTest that has been selected
+
+		//Get the center of lilB
+		Vector3 pos = lilB.center;
+
+		// Initialize the offset at [0, 0, 0]
+		Vector3 off = Vector3.zero;
+
+		switch (test) {
+
+		// The center test determines what off (offset) would have to be applied
+		// to lilB to move it's center back inside bigB
+		case BoundsTest.center:
+
+			// The center of lilB is in bigB, do nothing
+			if (bigB.Contains (pos)) {
+				return(Vector3.zero);
+			}
+
+			// Determines the x offset
+			if (pos.x > bigB.max.x) {
+				off.x = pos.x - bigB.max.x;
+			} else if (pos.x < bigB.min.x) {
+				off.x = pos.x - bigB.min.x;
+			}
+
+			// Determines the y offset
+			if (pos.y > bigB.max.y) {
+				off.y = pos.y - bigB.max.y;
+			} else if (pos.y < bigB.min.y) {
+				off.y = pos.y - bigB.min.y;
+			}
+
+			// Determines the z offset
+			if (pos.z > bigB.max.z) {
+				off.z = pos.z - bigB.max.z;
+			} else if (pos.z < bigB.min.z) {
+				off.z = pos.z - bigB.min.z;
+			}
+
+			return (off);
+
+		// The onScreen test determines what off would have to be applied to keep all of lilB inside bigB
+		case BoundsTest.onScreen:
+
+			// return [0, 0, 0]
+			if (bigB.Contains (lilB.min) && bigB.Contains (lilB.max)) {
+				return (Vector3.zero);
+			}
+
+			// X
+			if (lilB.max.x > bigB.max.x) {
+				off.x = lilB.max.x - bigB.max.x;
+			} else if (lilB.min.x < bigB.min.x) {
+				off.x = lilB.min.x - bigB.min.x;
+			}
+
+			// Y
+			if (lilB.max.y > bigB.max.y) {
+				off.y = lilB.max.y - bigB.max.y;
+			} else if (lilB.min.y < bigB.min.y) {
+				off.y = lilB.min.y - bigB.min.y;
+			}
+
+			// Z
+			if (lilB.max.z > bigB.max.z) {
+				off.z = lilB.max.z - bigB.max.z;
+			} else if (lilB.min.z < bigB.min.z) {
+				off.z = lilB.min.z - bigB.min.z;
+			}
+
+			return (off);
+
+		// The offScreen test determines what off would have to be applied to move any tiny part of lilB inside of bigB
+		case BoundsTest.offScreen:
+
+			bool cMin = bigB.Contains (lilB.min);
+			bool cMax = bigB.Contains (lilB.max);
+
+
+			// return [0, 0, 0]
+			if (cMin || cMax) {
+				return (Vector3.zero);
+			}
+
+			// X
+			if (lilB.min.x > bigB.max.x) {
+				off.x = lilB.min.x - bigB.max.x;
+			} else if (lilB.max.x < bigB.min.x) {
+				off.x = lilB.max.x - bigB.min.x;
+			}
+
+			// Y
+			if (lilB.min.y > bigB.max.y) {
+				off.y = lilB.min.y - bigB.max.y;
+			} else if (lilB.max.y < bigB.min.y) {
+				off.y = lilB.max.y - bigB.min.y;
+			}
+
+			// Z
+			if (lilB.min.z > bigB.max.z) {
+				off.z = lilB.min.z - bigB.max.z;
+			} else if (lilB.max.z < bigB.min.z) {
+				off.z = lilB.max.z - bigB.min.z;
+			}
+
+			return (off);
+
+		}
+
+		return(Vector3.zero);
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
