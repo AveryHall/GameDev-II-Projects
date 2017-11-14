d369ab9123c9bc95d90bb3ff07e2ac57df4929d7
Avery Hall <aphall@mail.lipscomb.edu>
2017-11-14 02:08:57 -0600

    Prospector Solitaire -- Baisc Card Functions

---
+++ Prospector Solitaire/Assets/__Scripts/Prospector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Prospector : MonoBehaviour {
+    static public Prospector    S;
+
+	[Header("Set in Inspector")]
+	public TextAsset            deckXML;
+
+	[Header("Set Dynamically")]
+    public Deck                 deck;
+
+    private void Awake() {
+        S = this; // Set up a Singelton for Prospector
+    }
+
+    private void Start() {
+        deck = GetComponent<Deck>(); // Get the Deck
+        deck.InitDeck(deckXML.text); // Pass DeckXML to it
+        Deck.Shuffle(ref deck.cards); // This shuffles the deck by reference  // a
+
+        Card c;
+        for (int cNum = 0; cNum < deck.cards.Count; cNum++) {  // b
+            c = deck.cards[cNum];
+            c.transform.localPosition = new Vector3((cNum % 13) * 3, cNum / 13 * 4, 0);
+
+        }
+    }
+
+}
