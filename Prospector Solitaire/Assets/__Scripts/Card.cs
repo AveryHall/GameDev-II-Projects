94325d46517c5bb12b4e847e84075cf6907efc23
Avery Hall <aphall@mail.lipscomb.edu>
2017-11-14 09:25:58 -0600

    Prospector Solitaire -- Almost Complete?

--- Prospector Solitaire/Assets/__Scripts/Card.cs
+++ Prospector Solitaire/Assets/__Scripts/Card.cs
@@ -18,6 +18,55 @@ public class Card : MonoBehaviour {
 
     public CardDefinition   def; // Parsed from DeckXML.xml
 
+    // List of the SpriteRenderer Components of this GameObject and its children 
+    public SpriteRenderer[] spriteRenderers;
+
+    void Start() {
+        SetSortOrder(0); // Ensures that the card starts properly depth sorted
+    }
+
+    // If SpriteRenders is not yet defined, this fucntion defines it
+    public void PopulateSpriteRenderers() {
+        // If spriteRenderers is null or empty
+        if(spriteRenderers == null || spriteRenderers.Length == 0) {
+            spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        }
+    }
+
+    // Sets the sortingLayerName on all SpriteRenderer Components
+    public void SetSortingLayerName(string tSLN) {
+        PopulateSpriteRenderers();
+
+        foreach(SpriteRenderer tSR in spriteRenderers) {
+            tSR.sortingLayerName = tSLN;
+        }
+    }
+
+    // Sets the sortingOrder of all SpriteRenderer Components
+    public void SetSortOrder(int sOrd) {
+        PopulateSpriteRenderers();
+
+        // Iterate through all the spriteRenderes as tSR
+        foreach(SpriteRenderer tSR in spriteRenderers) {
+            if (tSR.gameObject == this.gameObject) {
+                // If the gameObject is this.gameObject, it's the background
+                tSR.sortingOrder = sOrd; // Set it's order to sord
+                continue; // And continue to the next iteration of the loop
+            }
+            // Each of the children of this GameObject are named
+            // switch based on the names
+            switch (tSR.gameObject.name){
+                case "back": // if the name is "back"
+                    // Set it to the highest layer to cover the other sprites
+                    tSR.sortingOrder = sOrd + 2;
+                    break;
+                case "face" : // if the name is "face"
+                    tSR.sortingOrder = sOrd + 1;
+                    break;
+            }
+        }
+    }
+
     public bool faceUp {
         get {
             return (!back.activeSelf);
@@ -25,6 +74,11 @@ public class Card : MonoBehaviour {
         set {
             back.SetActive(!value);
         }
+    }
+
+    // Virtural methods can be overridden by subclass methods with the same name
+    virtual public void OnMouseUpAsButton() {
+        print(name); // When clicked, this outputs the card name
     }
 }
 
